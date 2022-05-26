66216673b242dd7a9b49b7755fddb2b9c57cb8d6
VSavin <v.savin@autodrive-agency.ru>
2022-05-26 16:03:06 +0300

    убрал лишний код

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,21 +19,6 @@
 </template>
 <script>
 export default {
-  emits:['isOpened'],
-  computed: {
-    username() {
-      // We will see what `params` is shortly
-      return this.$route.params.username
-    },
-  },
-  methods: {
-    goToDashboard() {
-      if (isAuthenticated) {
-        this.$router.push('/dashboard')
-      } else {
-        this.$router.push('/login')
-      }
-    },
-  },
+  emits:['isOpened']
 }
 </script>
